827896452ae2b8677f18800ff3d66e5738a93892
CinxZaned <affanarfani4@gmail.com>
2023-12-31 17:13:35 +0700

    update1

--- script.js
+++ script.js
@@ -19,44 +19,6 @@ btnCloseWindow.onclick = function() {
     modal.style.display = 'none';
 }
 
-// Variables to keep track of the mouse position
-var pos1 = 0,
-    pos2 = 0,
-    pos3 = 0,
-    pos4 = 0;
-
-// Function to start dragging
-function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    document.onmousemove = elementDrag;
-}
-
-// Function to drag the modal
-function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    theWindowA.style.top = (theWindowA.offsetTop - pos2) + "px";
-    theWindowA.style.left = (theWindowA.offsetLeft - pos1) + "px";
-}
-
-// Function to stop dragging
-function closeDragElement() {
-    document.onmouseup = null;
-    document.onmousemove = null;
-}
-
-// Attach the event listener for dragging to the header of the modal
-header.onmousedown = dragMouseDown;
-
-// Multiplication
 // ColA = RowB
 var colA = document.getElementById("y1");
 var rowB = document.getElementById("x2");
@@ -87,15 +49,39 @@ function generateTableA() {
     tableA.innerHTML = "";
 
     for(var i=0; i<(rowA.value-(-1)); i++) {
-        let tRowA = document.createElement('tr');
+        let tRowA;
 
-        for(var j=0; j<(colA.value-(-1)); j++) {
-            let cellA = document.createElement('td');
-            cellA.textContent = "Row " + (i + 1) + ", Col " + (j + 1);
-            tRowA.appendChild(cellA);
+        if (i==0) {
+            let tRowHead = document.createElement('tr');
+
+            for(var j=0; j<(colA.value-(-1)); j++) {
+                let tHeadA = document.createElement('th');
+                tHeadA.textContent = "th";
+                tRowHead.appendChild(tHeadA);
+            }
+            tableA.appendChild(tRowHead);
+
+        } else {
+            tRowA = document.createElement('tr');
+
+            for(var j=0; j<(colA.value-(-1)); j++) {
+                if (j==0) {
+                    let tRowHeadChild = document.createElement('th');
+                    tRowHeadChild.textContent = "th";
+                    tRowA.appendChild(tRowHeadChild);
+                } else {
+                    let cellA = document.createElement('td');
+                    let inputMatA = document.createElement('input');
+                    inputMatA.type = "text";
+                    inputMatA.name = "matA"+i+j;
+                    inputMatA.className = "inputMatA";
+                    inputMatA.title = `[${i},${j}]`;
+                    cellA.appendChild(inputMatA);
+                    tRowA.appendChild(cellA);
+                }
+            }
+            tableA.appendChild(tRowA);
         }
-
-        tableA.appendChild(tRowA);
     }
 }
 
@@ -103,14 +89,34 @@ function generateTableB() {
     tableB.innerHTML = "";
 
     for(var i=0; i<(rowB.value-(-1)); i++) {
-        let tRowB = document.createElement('tr');
+        let tRowB;
 
-        for(var j=0; j<(colB.value-(-1)); j++) {
-            let cellB = document.createElement('td');
-            cellB.textContent = "Row " + (i + 1) + ", Col " + (j + 1);
-            tRowB.appendChild(cellB);
+        if (i==0) {
+            let tRowHead = document.createElement('tr');
+
+            for(var j=0; j<(colB.value-(-1)); j++) {
+                let tHeadB = document.createElement('th');
+                tHeadB.textContent = "th";
+                tRowHead.appendChild(tHeadB);
+            }
+            tableB.appendChild(tRowHead);
+
+        } else {
+            tRowB = document.createElement('tr');
+
+            for(var j=0; j<(colB.value-(-1)); j++) {
+                if (j==0) {
+                    let tRowHeadChild = document.createElement('th');
+                    tRowHeadChild.textContent = "th";
+                    tRowB.appendChild(tRowHeadChild);
+                } else {
+                    let cellB = document.createElement('td');
+                    
+                    cellB.textContent = "td";
+                    tRowB.appendChild(cellB);
+                }
+            }
+            tableB.appendChild(tRowB);
         }
-
-        tableB.appendChild(tRowB);
     }
 }
